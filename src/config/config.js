13606de091c0417575c536e212e249c4cf6a76a1
Gonzalo09 <gonza.iglesias00@gmail.com>
2023-11-27 02:00:35 -0300

    Update apiUrl in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  // apiUrl: import.meta.env.VITE_APP_API_URL,
-  apiUrl: "http://localhost:3001",
+  apiUrl: import.meta.env.VITE_APP_API_URL,
+  // apiUrl: "http://localhost:3001",
 };
