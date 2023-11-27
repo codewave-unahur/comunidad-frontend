cb02e7de814fc0992c81e432857d0fa5c74c880f
Gonzalo09 <gonza.iglesias00@gmail.com>
2023-11-27 00:59:05 -0300

    Update API URL in .env and config.js files

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,4 @@
 export const config = {
-  //apiUrl: import.meta.env.VITE_APP_API_URL,
-
+  // apiUrl: import.meta.env.VITE_APP_API_URL,
   apiUrl: "http://localhost:3001",
-  //apiUrl: 'https://unahur-equivalencias-backend.herokuapp.com/api'
 };
