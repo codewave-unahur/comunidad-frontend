0ca945ffaa17ce9ea11c7ac936efb1cfffbd5ca5
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2023-12-13 09:53:05 -0300

    Update files_service.js

--- src/services/files_service.js
+++ src/services/files_service.js
@@ -73,3 +73,27 @@ export async function uploadCV(cv, id, token) {
     console.log(error);
   }
 }
+
+export async function uploadCUD(cud, id, token) {
+  const formData = new FormData();
+  formData.append("uploadCUD", cud);
+
+  const pedido = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: `${config.apiUrl}/files/cud/?authorization=${token}`,
+    headers: {
+      Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqanJ4aGNlcmpqdGhqZ2xxcHRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUyNjQ2NywiZXhwIjoyMDA5MTAyNDY3fQ.aJ54McCK2fK2Oac-hmGkXWfXZHYy5AiQ4GC_-W5ze8Y",
+      id: id,
+      },
+      data: formData,
+    };
+
+    try{
+      const response = await axios.request(pedido);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
