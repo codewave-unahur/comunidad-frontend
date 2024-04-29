3bc2c608e49851243058220891c928cddd34b599
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-04-29 12:55:13 -0300

    Update Empresas.jsx

--- src/components/Perfiles/PerfilAdministrador/Empresas.jsx
+++ src/components/Perfiles/PerfilAdministrador/Empresas.jsx
@@ -22,7 +22,6 @@ import {
 import { getEmpresas, putEmpresa } from "../../../services/empresas_service";
 import { useEffect, useState, forwardRef } from "react";
 import Buscador from "../../Buscador/Buscador";
-import { putUsuario } from "../../../services/usuarios_service";
 
 import { Toaster, toast } from "sonner";
 import Paginacion from "../../Paginacion/Paginacion";
@@ -79,18 +78,11 @@ const Empresas = () => {
       idEstado: 1,
     };
 
-    const usuarioActivo = {
-      estado: true,
-    }
-   
     try {
       const response = await putEmpresa(id, estadoActivo, token);
-      const activarUsuario = await putUsuario(id, usuarioActivo, token)
-
       if (response) {
         toast.success("La empresa fue activada con éxito.");
-        setEmpresas(empresas.filter((empresa) => empresa.id !== id), estadoActivo);
-        setUsuario(empresas.map((empresa) => empresa.Usuario.id !== id), usuarioActivo);
+        setEmpresas(empresas.filter((empresa) => empresa.id !== id));
         setOpen(false);
       } else {
         toast.error("No se pudo activar la empresa.");
@@ -99,19 +91,6 @@ const Empresas = () => {
       toast.error("No se pudo activar la empresa.");
     }
   };
-
-  const activarUsuario = (id) => async () => {
-    const estadoActivo = {
-      estado: true,
-    };
-
-    try {
-      const response = await setUsuario(id, estadoActivo, token);
-
-      if (response) {
-        toast.success("El usuario fue activado con éxito.");
-        
-
 
   const handleSubmit = async (e, buscador) => {
     e.preventDefault();
