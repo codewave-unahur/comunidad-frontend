40953a1029c38bee992c85f8ff4cc6af5bececed
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-05-22 17:05:10 -0300

    Agregados links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,99 +20,112 @@ export default function Footer() {
         <Box
           sx={{
             display: "flex",
-            flexDirection:"row",
-            justifyContent: "space-evenly",
+            flexDirection: "row",
+            justifyContent: "space-between",
             alignItems: "center",
             backgroundColor: "#F4F4F4",
-            padding: { xs: 2, sm: "60px", md: "120px"}
+            padding: { xs: 2, sm: "60px", md: "120px" },
           }}
         >
-        <Grid container spacing={2} sx={{display:"flex", justifyContent:"space-between"}}>
-            <Grid item xs={12} sm={4}>
-                <Box>
+          <Grid
+            container
+            spacing={2}
+            sx={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", gap: 2}}
+          >
+            <Grid item xs={12} sm={3}>
+              <Box>
                 <img
-                    src={logocpyme}
-                    alt="logo cpyme"
-                    style={{ width: "50%", height: "50%" }}
+                  src={logocpyme}
+                  alt="logo cpyme"
+                  style={{ width: "50%", height: "50%" }}
                 />
                 <p style={{ color: "#636664", fontSize: "11.6px" }}>
-                    &copy; {year} Direccion de Vinculación Tecnológica - UNAHUR
+                  &copy; {year} Direccion de Vinculación Tecnológica - UNAHUR
                 </p>
                 <p style={{ color: "#636664", fontSize: "11.6px" }}>
-                    Todos los derechos reservados
+                  Todos los derechos reservados
                 </p>
-    
+
                 <Link href="#" style={{ color: "#636664", fontSize: "11.6px" }}>
-                    <p>
-                    Terminos legales
-                    </p>
+                  <p>Terminos legales</p>
                 </Link>
                 <Link href="#" style={{ color: "#636664", fontSize: "11.6px" }}>
-                    <p>
-                    Aviso de cookies
-                    </p>
+                  <p>Aviso de cookies</p>
                 </Link>
-                
+
                 <Box
-                    sx={{
-                    display: "column",
-                    justifyContent: "center",
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
                     alignItems: "center",
-                    }}
+                    gap: 1,
+                  }}
                 >
-                    <Link href="#">
+                  <Link
+                    href="https://www.instagram.com/cpymeunahur/"
+                    target="__blank"
+                  >
                     <InstagramIcon sx={{ color: "#5C5A5E" }} />
-                    </Link>
-                    <Link href="#">
+                  </Link>
+                  <Link
+                    href="https://www.facebook.com/cpymeUNAHUR"
+                    target="__blank"
+                  >
                     <FacebookIcon sx={{ color: "#5C5A5E" }} />
-                    </Link>
-                    <Link href="#">
-                    <TwitterIcon sx={{ color: "#5C5A5E" }}/>
-                    </Link>
-                    <Link href="#">
-                    <LinkedIn sx={{ color: "#5C5A5E" }}/>
-                    </Link>
-                    <Link href="#">
+                  </Link>
+                  <Link
+                    href="https://twitter.com/cpymeUNAHUR/"
+                    target="__blank"
+                  >
+                    <TwitterIcon sx={{ color: "#5C5A5E" }} />
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/company/centro-pyme-unahur/"
+                    target="__blank"
+                  >
+                    <LinkedIn sx={{ color: "#5C5A5E" }} />
+                  </Link>
+                  <Link
+                    href="https://www.youtube.com/@cpymeunahur"
+                    target="__blank"
+                  >
                     <YouTubeIcon sx={{ color: "#5C5A5E" }} />
-                    </Link>
+                  </Link>
                 </Box>
-                </Box>
+              </Box>
             </Grid>
-            <Grid item xs={12} sm={4}>
-    
-          <Box>
-            <MapaCpyme />
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-            <Box>
+            <Grid item xs={12} sm={5}>
+              <Box>
+                <MapaCpyme />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Box>
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-                <PlaceIcon sx={{ color: "#5C5A5E" }} />
-                <p style={{ color: "#636664", fontSize: "11.6px" }}>
+                  <PlaceIcon sx={{ color: "#5C5A5E" }} />
+                  <p style={{ color: "#636664", fontSize: "11.6px" }}>
                     Ausebione 8, B1688 Villa Tesei, Provincia de Buenos Aires
-                </p>
+                  </p>
                 </Box>
-                
+
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-                <EmailIcon sx={{ color: "#5C5A5E" }} />
-                <p style={{ color: "#636664", fontSize: "11.6px" }}>
+                  <EmailIcon sx={{ color: "#5C5A5E" }} />
+                  <p style={{ color: "#636664", fontSize: "11.6px" }}>
                     empleabilidad@unahur.edu.aria
-                </p>
+                  </p>
                 </Box>
-                
+
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-                <EmailIcon sx={{ color: "#5C5A5E" }} />
-                <p style={{ color: "#636664", fontSize: "11.6px" }}>
+                  <EmailIcon sx={{ color: "#5C5A5E" }} />
+                  <p style={{ color: "#636664", fontSize: "11.6px" }}>
                     dvt@unahur.edu.ar
-                </p>
+                  </p>
                 </Box>
-            </Box>
-        </Grid>
-        </Grid>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
-        </Box>
-        
-          
+      </Box>
     </>
   );
 }
