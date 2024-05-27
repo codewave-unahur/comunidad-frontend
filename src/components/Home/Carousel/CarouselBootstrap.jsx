72897e566d3c96ee0aa4e35d62fa3279eae7157f
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-05-27 16:13:52 -0300

    agregadas imagenes de ejemplo

--- src/components/Home/Carousel/CarouselBootstrap.jsx
+++ src/components/Home/Carousel/CarouselBootstrap.jsx
@@ -7,7 +7,7 @@ const CarouselBootstrap = () => {
       <Carousel.Item>
         <img
           className="w-100 h-auto" 
-          src="https://i.pinimg.com/736x/9c/7b/e4/9c7be43979a736a8695361a544630b97.jpg"
+          src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/73e4073787e3f71b03a981d7db81a660/abanderadocampus.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
@@ -19,7 +19,7 @@ const CarouselBootstrap = () => {
       <Carousel.Item>
         <img
           className="w-100 h-auto"
-          src="https://i.pinimg.com/736x/9c/7b/e4/9c7be43979a736a8695361a544630b97.jpg"
+          src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/c25002faae1e80623ff82afc991fc897/clases-de-apoyo-campus_nuevo.jpg"
           alt="Second slide"
         />
         <Carousel.Caption>
@@ -27,7 +27,18 @@ const CarouselBootstrap = () => {
           <p>Descripción de la segunda diapositiva.</p>
         </Carousel.Caption>
       </Carousel.Item>
-
+      <Carousel.Item>
+        <img
+          className="w-100 h-auto"
+          src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/e4a64014ee84273e7e562fa76905ff4e/dudas-gestion-estudiantilcampus-1.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Tercera diapositiva</h3>
+          <p>Descripción de la tercera diapositiva.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
     </Carousel>
   );
 }
