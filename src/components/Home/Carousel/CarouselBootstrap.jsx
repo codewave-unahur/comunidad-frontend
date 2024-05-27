488ed44925edac8278a3714c810d8c7f561bebcc
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-05-27 16:36:35 -0300

    cambios classname en css login

--- src/components/Home/Carousel/CarouselBootstrap.jsx
+++ src/components/Home/Carousel/CarouselBootstrap.jsx
@@ -3,42 +3,33 @@ import Carousel from "react-bootstrap/Carousel";
 
 const CarouselBootstrap = () => {
   return (
-    <Carousel>
+
+    <Carousel className="d-flex justify-content-center">
       <Carousel.Item>
         <img
-          className="w-100 h-auto" 
+          className="d-block w-100 " 
           src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/73e4073787e3f71b03a981d7db81a660/abanderadocampus.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>Primera diapositiva</h3>
-          <p>Descripción de la primera diapositiva.</p>
-        </Carousel.Caption>
+        
       </Carousel.Item>
 
       <Carousel.Item>
         <img
-          className="w-100 h-auto"
+          className="d-block w-100 "
           src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/c25002faae1e80623ff82afc991fc897/clases-de-apoyo-campus_nuevo.jpg"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Segunda diapositiva</h3>
-          <p>Descripción de la segunda diapositiva.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="w-100 h-auto"
+          className="d-block w-100 "
           src="https://campus.unahur.edu.ar/wp-content/uploads/slider/cache/e4a64014ee84273e7e562fa76905ff4e/dudas-gestion-estudiantilcampus-1.jpg"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h3>Tercera diapositiva</h3>
-          <p>Descripción de la tercera diapositiva.</p>
-        </Carousel.Caption>
+        
       </Carousel.Item>
-      
+
     </Carousel>
   );
 }
