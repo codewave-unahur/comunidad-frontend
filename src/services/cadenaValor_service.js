e169c523eef2363f83778d0a70db1857b596ee48
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-05-28 12:40:04 -0300

    agregado rubro y cadena de valor al registro

--- src/services/cadenaValor_service.js
+++ src/services/cadenaValor_service.js
@@ -3,9 +3,9 @@ import {config} from "../config/config";
 
 // Trae todas las cadenas de valor
 
-export async function getCadenasValor() {
+export async function getCadenaValor() {
     try {
-        const response = await axios.get(`${config.apiUrl}/cadenasValor`);
+        const response = await axios.get(`${config.apiUrl}/cadenaValor`);
         return response.data;
     } catch (error) {
         console.error(error);
