64f077ee1121c5b545cdc961511168ea9d7c098c
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-06-07 14:12:40 -0300

    Update Galeria.jsx

--- src/components/Perfiles/PerfilAdministrador/Galeria.jsx
+++ src/components/Perfiles/PerfilAdministrador/Galeria.jsx
@@ -1,77 +1,225 @@
-import { Box, Button, Card, CardHeader } from '@mui/material'
-import React, { useEffect } from 'react'
-import { uploadImage } from '../../../services/galeria_service'
-import { useState } from 'react'
-import { getGaleria } from '../../../services/galeria_service'
-import { deleteGaleria } from '../../../services/galeria_service'
-import { toast } from 'sonner'
+import { Box, Slide, Button, Card, MenuItem, ListItemIcon, CardHeader, Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Typography, Dialog, DialogTitle, DialogContent, Stack, DialogActions } from "@mui/material";
+import React, { useEffect, forwardRef } from "react";
+import { uploadImage } from "../../../services/galeria_service";
+import { useState } from "react";
+import { getGaleria } from "../../../services/galeria_service";
+import { deleteGaleria } from "../../../services/galeria_service";
+import { Toaster, toast } from "sonner";
+import ImageIcon from '@mui/icons-material/Image';
 
 export default function Galeria() {
-
-  const [galeria, setGaleria] = useState([])
-
+  const [galeria, setGaleria] = useState([]);
+  const [isImageSelected, setIsImageSelected] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
 
 
   useEffect(() => {
     const fetchGaleria = async () => {
-      const response = await getGaleria()
+      const response = await getGaleria();
       if (response) {
-        setGaleria(response.carouselImages)
+        setGaleria(response.carouselImages);
       }
-    }
-    fetchGaleria()
-  }, []) 
+    };
+    fetchGaleria();
+  }, []);
 
-  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setImagenSeleccionada(null);
+    setOpen(false);
+    setIsImageSelected(false);
+  };
+
+  const handleImagenSeleccionada = (e) => {
+    setImagenSeleccionada(e.target.files[0]);
+    setIsImageSelected(true);
+  };
+
+  const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
   const handleUploadImage = async (image) => {
     try {
-      const response = await uploadImage(image)
+      
+      const response = await uploadImage(imagenSeleccionada);
       if (response) {
-        toast.success("Imagen subida correctamente")
+        toast.success("Imagen subida correctamente");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+
       }
+    } catch (error) {
+      toast.error("Error al subir la imagen");
     }
-    catch (error) {
-      console.log(error)
-    }
-  }
+  };
 
   const handleDeleteImage = async (id) => {
     try {
-      const response = await deleteGaleria(id)
+      const response = await deleteGaleria(id);
       if (response) {
-        toast.success("Imagen eliminada correctamente")
-        setGaleria(galeria.filter(image => image.id !== id))
+        toast.success("Imagen eliminada correctamente");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
       }
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
+  };
+
+  const convertirHora = (hora) => {
+    const date = new Date(hora);
+    return date.toLocaleString();
   }
-
-
-
-    
 
   return (
     <>
       <Card type="section" elevation={8}>
         <CardHeader title="Galería" />
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center" sx={{ width: "20%" }}>
+                  <Typography variant="h5">
+                    Imagen
+                  </Typography> 
+                </TableCell>
+                <TableCell align="center" sx={{ width: "20%" }}>
+                  <Typography variant="h5">ID</Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "20%" }}>
+                  <Typography variant="h5"> Fecha de subida </Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "20%" }}>
+                  <Typography variant="h5">Acciones</Typography>
+                </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+              {galeria.map((image, index) => (
+                <TableRow key={image.id}>
+                  <TableCell align="center" sx={{ width: "20%" }}>
+                    <img
+                      src={image.imageUrl}
+                      alt="imagen"
+                      style={{ width: "100px" }}
+                    />
+                  </TableCell>
+                  <TableCell align="center" sx={{ width: "20%" }}>
+                    <Typography variant="h5">{image.id}</Typography>
+                  </TableCell>
+                  <TableCell align="center" sx={{ width: "20%" }}>
+                    <Typography variant="h5">
+                      {convertirHora(image.createdAt)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="center" sx={{ width: "20%" }}>
+                    <Button onClick={() => handleDeleteImage(image.id)} variant="contained" color="error">
+                      Eliminar
+                    </Button>
+                  </TableCell>
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <MenuItem
+                  key="uploadImage"
+                  onClick={() => {
+                    handleClickOpen();
+                  }}
+                >
+                  <ListItemIcon>
+                    <ImageIcon />
+                  </ListItemIcon>
+                  Subir imagen a galeria
+                </MenuItem>,
         <Box>
-          {galeria.map((image, index) => ( 
-            <Box key={index}>
-              <img src={image.imageUrl} alt="imagen" style={{width: '100px'}} />
-              <Button onClick={() => handleDeleteImage(image.id)}>Eliminar</Button>
-            </Box>
-
-          ))}
+          <Dialog key="uploadImage" 
+          open={open} 
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+          maxWidth="sm"
+          fullWidth >
+            <DialogTitle>
+              {"Por favor seleccione la imagen que desea subir a la galeria (max 1000x1000)"}
+            </DialogTitle>
+            <DialogContent>
+              <Stack
+              direction={{ xs: "column", sm: "column", md: "row" }}
+              spacing={6}
+              paddingY={2}
+              alignItems="center"
+              justifyContent="center"
+              >
+                <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          paddingX: 2,
+                        }}
+                      >
+                        <Button
+                          component="label"
+                          disableElevation
+                          size="medium"
+                          variant="contained"
+                          endIcon={<ImageIcon />}
+                          sx={{
+                            marginTop: 1,
+                          }}
+                        >Seleccione su imagen
+                        <input
+                          type="file"
+                          onChange={handleImagenSeleccionada}
+                          hidden
+                        /></Button>
+                         {isImageSelected && (
+                          <Typography
+                            variant="subtitle1"
+                            display="block"
+                            sx={{
+                              marginTop: 1,
+                            }}
+                          >
+                            {imagenSeleccionada.name}
+                          </Typography>
+                        )}
+                        </Box>
+                        </Stack>
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={handleClose} color="error">
+                            Cancelar
+                          </Button>
+                          <Button
+                            onClick={handleUploadImage}
+                            color="success"
+                            variant="contained"
+                            disableElevation
+                            disabled={!isImageSelected}
+      
+                          >
+                            Subir imagen
+                          </Button>
+                        </DialogActions>
+                      </Dialog>
+                      <Toaster key="subirImagenToaster" richColors closeButton />
+          
+                  
         </Box>
-
-        
-
-        
       </Card>
-
     </>
-  )
+  );
 }
