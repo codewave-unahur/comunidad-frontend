7cc74ab410fd2a97e20d4afbb1a4f760ecc2edfa
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-07-03 14:19:40 -0300

    agregado token a galeria

--- src/services/files_service.js
+++ src/services/files_service.js
@@ -12,6 +12,7 @@ export async function uploadLogo(logo, id) {
     headers: {
       //no se como pasar este token al env pero bueno, va en el env
       Authorization: `bearer ${sessionStorage.getItem("token")}`,
+      id: id,
     },
     data: formData,
   };
@@ -34,6 +35,7 @@ export async function uploadFoto(foto, id, token) {
     url: `${config.apiUrl}/files/foto/?authorization=${token}`,
     headers: {
       Authorization: `bearer ${sessionStorage.getItem("token")}`,
+      id: id,
     },
     data: formData,
   };
@@ -56,6 +58,7 @@ export async function uploadCV(cv, id, token) {
     url: `${config.apiUrl}/files/cv/?authorization=${token}`,
     headers: {
       Authorization: `bearer ${sessionStorage.getItem("token")}`,
+      id: id
     },
     data: formData,
   };
@@ -78,7 +81,7 @@ export async function uploadCUD(cud, id, token) {
     url: `${config.apiUrl}/files/cud/?authorization=${token}`,
     headers: {
       Authorization:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqanJ4aGNlcmpqdGhqZ2xxcHRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUyNjQ2NywiZXhwIjoyMDA5MTAyNDY3fQ.aJ54McCK2fK2Oac-hmGkXWfXZHYy5AiQ4GC_-W5ze8Y",
+        `bearer ${sessionStorage.getItem("token")}`,
       id: id,
       },
       data: formData,
