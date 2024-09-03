c13fc2419899e9ac72562ebf6aaf2f1e90bc6f19
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-09-03 15:53:37 -0300

    cambio de rutas para buscador

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
     <Routes>
       
       <Route path="/" element={<Inicio />} />
+      <Route path="/home" element={<Inicio />} />
       <Route path="/perfil" element={
         <Perfil />
       } />
