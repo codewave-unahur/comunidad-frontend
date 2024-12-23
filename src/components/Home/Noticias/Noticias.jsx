4d00865364cfca1470f54f9cabce2c348379caca
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-12-23 10:11:44 -0300

    Arreglada animacion de las cards

--- src/components/Home/Noticias/Noticias.jsx
+++ src/components/Home/Noticias/Noticias.jsx
@@ -2,46 +2,188 @@ import React, { useState } from "react";
 import { Card, CardMedia, Box, Typography, Button } from "@mui/material";
 
 const CustomCard = () => {
-  const [hovered, setHovered] = useState(false);
 
   return (
     <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          marginBottom: "20px",
+        }}
+      >
         <Card sx={{ width: 290, position: "relative" }}>
-        {/* Imagen */}
-        <CardMedia
+          {/* Imagen */}
+          <CardMedia
             component="img"
             image="https://via.placeholder.com/290x180"
             alt="Imagen"
             sx={{ width: 290, height: 180 }}
-        />
-        {/* Texto superpuesto en la parte inferior */}
-        <Box
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
+          />
+          <Box
+            
             sx={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            width: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.6)", // Fondo negro con opacidad
-            color: hovered ? "yellow" : "white", // Cambia el color del texto al pasar el mouse
-            padding: "8px",
-            textAlign: "center",
-            transition: "color 0.3s ease",
-            cursor: "pointer",
+              position: "absolute",
+              left: 0,
+              right: 0,
+              bottom: 0,
+              margin: "8px 8px 0px 8px",
+              backgroundColor: "rgba(0, 0, 0, 0.6)", // Fondo negro con opacidad
+              padding: "8px",
+              textAlign: "left",
+              transition: "color 0.3s ease",
+              color:"white",
+              
+              cursor: "pointer",
+              '&:hover .hover-text': { color: 'lightGreen',  }
             }}
-        >
-            <Typography variant="h6">Texto interactivo</Typography>
-        </Box>
+          >
+            <Typography
+              className="hover-text"
+              variant="h6"
+              sx={{
+                fontSize: "16px",
+                transition: "color 0.3s ease-in-out"
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing aliquid
+              provident ?
+            </Typography>
+          </Box>
         </Card>
+        <Card sx={{ width: 290, position: "relative" }}>
+          {/* Imagen */}
+          <CardMedia
+            component="img"
+            image="https://via.placeholder.com/290x180"
+            alt="Imagen"
+            sx={{ width: 290, height: 180 }}
+          />
+          <Box
+            
+            sx={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              bottom: 0,
+              margin: "8px 8px 0px 8px",
+              backgroundColor: "rgba(0, 0, 0, 0.6)", // Fondo negro con opacidad
+              padding: "8px",
+              textAlign: "left",
+              transition: "color 0.3s ease",
+              color:"white",
+              
+              cursor: "pointer",
+              '&:hover .hover-text': { color: 'lightGreen',  }
+            }}
+          >
+            <Typography
+              className="hover-text"
+              variant="h6"
+              sx={{
+                fontSize: "16px",
+                transition: "color 0.3s ease-in-out"
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing aliquid
+              provident ?
+            </Typography>
+          </Box>
+        </Card>
+        <Card sx={{ width: 290, position: "relative" }}>
+          {/* Imagen */}
+          <CardMedia
+            component="img"
+            image="https://via.placeholder.com/290x180"
+            alt="Imagen"
+            sx={{ width: 290, height: 180 }}
+          />
+          <Box
+            
+            sx={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              bottom: 0,
+              margin: "8px 8px 0px 8px",
+              backgroundColor: "rgba(0, 0, 0, 0.6)", // Fondo negro con opacidad
+              padding: "8px",
+              textAlign: "left",
+              transition: "color 0.3s ease",
+              color:"white",
+              
+              cursor: "pointer",
+              '&:hover .hover-text': { color: 'lightGreen',  }
+            }}
+          >
+            <Typography
+              className="hover-text"
+              variant="h6"
+              sx={{
+                fontSize: "16px",
+                transition: "color 0.3s ease-in-out"
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing aliquid
+              provident ?
+            </Typography>
+          </Box>
+        </Card>
+        <Card sx={{ width: 290, position: "relative" }}>
+          {/* Imagen */}
+          <CardMedia
+            component="img"
+            image="https://via.placeholder.com/290x180"
+            alt="Imagen"
+            sx={{ width: 290, height: 180 }}
+          />
+          <Box
+            
+            sx={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              bottom: 0,
+              margin: "8px 8px 0px 8px",
+              backgroundColor: "rgba(0, 0, 0, 0.6)", // Fondo negro con opacidad
+              padding: "8px",
+              textAlign: "left",
+              transition: "color 0.3s ease",
+              color:"white",
+              
+              cursor: "pointer",
+              '&:hover .hover-text': { color: 'lightGreen',  }
+            }}
+          >
+            <Typography
+              className="hover-text"
+              variant="h6"
+              sx={{
+                fontSize: "16px",
+                transition: "color 0.3s ease-in-out"
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing aliquid
+              provident ?
+            </Typography>
+          </Box>
+        </Card>
+      </Box>
+      <Box sx={{
+        display:"flex",
+        justifyContent:"center"
         
-        <Button variant="outlined" sx={{
-            color:"black"
+        
+      }}>
+        <Button variant="outlined" 
+        sx={{
+            color:"#281ac2"
         }}>
-            MÁS NOTICIAS
+            MAS NOTICIAS
         </Button>
+      </Box>
     </>
-
   );
 };
 
