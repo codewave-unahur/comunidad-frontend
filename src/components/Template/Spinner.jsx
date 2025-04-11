23886da66790fcf68d3eec558c40c9995c06205d
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2025-04-11 15:52:32 -0300

    cambio color spinner

--- src/components/Template/Spinner.jsx
+++ src/components/Template/Spinner.jsx
@@ -4,28 +4,27 @@ import Box from '@mui/material/Box';
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "8rem"
-      }}>
-        <Box sx={{ 
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100vw',
-            position: 'absolute',
-            zIndex: 9999,
-            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-        }}>
-            <CircularProgress sx={{
-                    color: '#00404f',
-                    width: '100px',
-                    height: '100px',
-            }} />
-        </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)', // Fondo semitransparente
+        zIndex: 1300, // Asegura que esté por encima de otros elementos
+      }}
+    >
+      <CircularProgress
+        sx={{
+          color: '#2e7d32', // Color verde claro
+          width: '80px',
+          height: '80px',
+        }}
+      />
     </Box>
   );
 }
