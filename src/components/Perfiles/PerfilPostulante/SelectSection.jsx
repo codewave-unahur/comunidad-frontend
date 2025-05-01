57b1551269a42caf6452094e4643a6178796dbbe
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2025-05-01 12:57:01 -0300

    agregadas imagenes

--- src/components/Perfiles/PerfilPostulante/SelectSection.jsx
+++ src/components/Perfiles/PerfilPostulante/SelectSection.jsx
@@ -1,16 +1,70 @@
-import { Card, CardHeader, Box } from '@mui/material'
-import React from 'react'
+import { Card, CardHeader, Box, Typography } from '@mui/material';
+import React from 'react';
+import jobs from '../../../assets/jobs.svg';
+import news from '../../../assets/news.svg';
 
 export default function SelectSection() {
   return (
     <>
-        <Card type="section" elevation={8}>
-            <CardHeader title="Selecciona" />
-            <Box>
+      <Card type="section" elevation={8}>
+        <CardHeader title="Selecciona" />
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', sm: 'row' },
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              padding: '1rem',
+              gap: { xs: '1rem', sm: '2rem' },
+            }}
+          >
 
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <img
+                src={jobs}
+                alt="jobs"
+                style={{
+                  width: '400px',
+               
+                  height: '400px',
+                }}
+              />
+              <Typography variant="h5" align="center">
+                Ofertas
+              </Typography>
             </Box>
-                
-        </Card>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <img
+                src={news}
+                alt="news"
+                style={{
+                  width: '400px',
+               
+                  height: '400px',
+                }}
+              />
+              <Typography variant="h5" align="center">
+                Noticias
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Card>
     </>
-  )
+  );
 }
